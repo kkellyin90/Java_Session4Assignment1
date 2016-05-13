36d764444b5f68f19ba5bc24a70980e3d5b516f4
kkellyin90 <keith.kelly.a@gmail.com>
2016-05-13 19:19:25 +0530

    Add files via upload

---
+++ reverseArry.java
@@ -0,0 +1,14 @@
+package session4Assignment1;
+
+public class reverseArry {
+
+	public static void main(String[] args) {
+		int []arr1 = {1,2,3,4,5,6,7,8,9};
+		
+		for(int i= arr1.length - 1; i >= 0; i--){
+			System.out.print("\t"+ arr1[i]);
+		}
+
+	}
+
+}
